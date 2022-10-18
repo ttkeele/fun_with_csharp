3b84fa94c84b23958a735b5272d44c3110d9cc22
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-10-18 14:58:47 -0700

    while...

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -80,7 +80,7 @@ namespace HelloWorld
             if (b > a) ;
             Console.WriteLine("b is greater than a");
 
-            int time = 16;
+            int time = 18;
             if (time <= 16)
             {
                 Console.WriteLine("Good Day.");
@@ -88,7 +88,54 @@ namespace HelloWorld
             else
             {
                 Console.WriteLine("Good Evening.");
-            }        
-       }
+            }
+            int month = 10;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("Febuary");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+
+            }
+            int i = 0;
+            while (i < 7)
+            {
+                Console.WriteLine("I Like Shrimp");
+                i++;
+            }
+        }
     }
 }
